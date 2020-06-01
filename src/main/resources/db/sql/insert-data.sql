a675d6078ab07509a46390b38a09ca0fe4c5eb51
Mamatha <mamatha.k@al-enterprise.com>
2020-06-01 18:24:33 +0530

    App properties

--- src/main/resources/db/sql/insert-data.sql
+++ src/main/resources/db/sql/insert-data.sql
@@ -4,3 +4,4 @@ VALUES ('1', 'Rose', '2', '10', '5');
 INSERT INTO Flowers (flowerId, flowerName, noOfFlowers, costOfFlowers, discount)
 VALUES ('2', 'Lilly', '2', '10', '5');
 
+Select * from Flowers;
